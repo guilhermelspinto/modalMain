4248b922d179859b683f03293025f08cbfb053a2
Guilherme Lopes Sousa Pinto <guilherme.lopes@curriculointerativo.com.br>
2023-06-30 06:50:45 -0300

    Ajustado javascript

--- modal-main.js
+++ modal-main.js
@@ -1,164 +1,226 @@
-class modalMainTemplates {
+class ModalMainTemplates {
+    constructor() {
+        this.body = "";
+        this.error = "";
+    }
+
     alertMessage(template) {
         this.body = `
-        <div class="modal-main-message">
-            ${template}
-        </div>
-        `
+      <div class="modal-main-message">
+        ${template}
+      </div>
+    `;
+    }
+
+    setBody(template) {
+        this.body = `
+      ${template}
+    `;
     }
 
     tableItem(data) {
-
-        if(typeof data != "object"){
-            throw new Error('function tableItem\'s param must be an array');
-            this.error = "function tableItem\'s param must be an array"
-            return
+        if (!Array.isArray(data)) {
+            throw new Error("The parameter of the tableItem function must be an array");
         }
 
-        let template = `
-        <div class="modal-main-table-itens">`
-
-        data.map(function(e){
-            template += `
-            <div class="modal-main-table-item">
-                <span>${e.title}</span>
-                <span>${e.data}</span>
-            </div>`
-        })
-
-        template += `</div>`
-
-         this.body = template
+        this.body = `
+      <div class="modal-main-table-itens">
+        ${data.map(({title, data}) => `
+          <div class="modal-main-table-item">
+            <span>${title}</span>
+            <span>${data}</span>
+          </div>
+        `).join("")}
+      </div>
+    `;
     }
 
     multipleTemplate(templateArr) {
-
-        if(typeof templateArr != "object"){
-            throw new Error('function multipleTemplate\'s param must be an array');
-            this.error = "function multipleTemplate\'s param must be an array"
-            return
+        if (!Array.isArray(templateArr)) {
+            throw new Error("The parameter of the multipleTemplate function must be an array");
         }
 
-        let template = ``
-
-        templateArr.map(function(e){
-            template += e
-        })
-
-        this.body = template
+        this.body = templateArr.join("");
     }
 }
 
-class modalMain extends modalMainTemplates {
 
-    target = document.querySelector("body")
-
-    error = ""
-
-    template = ""
-    title = ""
-
-    templateNode = ""
-    body = ""
-
-    buttons = ""
+class ModalMain extends ModalMainTemplates {
+    constructor() {
+        super();
+        this.target = document.querySelector("body");
+        this.template = "";
+        this.closeOnClick = true;
+        this.title = "";
+        this.templateNode = "";
+        this.buttons = null;
+    }
 
     create() {
-        let templateCreate = `
-                <div class="modal-main opacity-0">
-                    <header>
-                        <span>${this.title}</span>
-                        <div class="close-modal-container">
-                            <span style="font-family: monospace">X</span>
-                        </div>
-                    </header>
-                </div>
-`
+        const templateCreate = `
+      <div class="modal-main modal-opacity-opacity-0">
+        <header>
+          <span>${this.title}</span>
+          <div class="close-modal-container">
+            <span style="font-family: monospace">X</span>
+          </div>
+        </header>
+      </div>
+    `;
 
-        this.template = templateCreate
+        this.template = templateCreate;
 
-        this.templateNode = document.createElement("div")
-        this.templateNode.classList.add("box-shadow")
-        this.templateNode.innerHTML = this.template
+        this.templateNode = document.createElement("div");
+        this.templateNode.classList.add("box-shadow");
+        this.templateNode.innerHTML = this.template;
 
         if (this.body) {
-            let bodyContainer = document.createElement("div")
-            bodyContainer.classList.add("modal-main-container")
-            bodyContainer.innerHTML = this.body
 
-            this.templateNode.querySelector(".modal-main").appendChild(bodyContainer)
+            const bodyContainer = document.createElement("div");
+            bodyContainer.classList.add("modal-main-container");
+            bodyContainer.innerHTML = this.body;
+
+            this.templateNode.querySelector(".modal-main").appendChild(bodyContainer);
         }
 
-        this.setButtons()
+        this.setButtons();
 
-        this.show(this.templateNode)
+        this.show(this.templateNode);
     }
 
     setButtons() {
-        let obj = this
+        const buttonsContainer = document.createElement("div");
+        buttonsContainer.classList.add("buttons-container");
+        this.templateNode.querySelector(".modal-main").appendChild(buttonsContainer);
 
-        let buttonsContainer = document.createElement("div")
-        buttonsContainer.classList.add("buttons-container")
-        obj.templateNode.querySelector(".modal-main-container").appendChild(buttonsContainer)
+        const buttons = this.buttons || [{
+            type: "Cancel",
+            title: "Ok",
+            clicked: this.close.bind(this)
+        }];
 
-        if (this.buttons) {
-            obj.buttons.map(function (e, i) {
-                //cria node botão coloca a classe o texto e a função
-                let buttonNode = document.createElement("button")
-                buttonNode.classList.add(e.type)
-                buttonNode.innerHTML = e.title
-                if(typeof e.clicked === "function"){
-                    buttonNode.onclick = e.clicked
+        buttons.forEach(({type, title, clicked, attr}) => {
+            const buttonNode = document.createElement("button");
+
+            buttonNode.classList.add(type);
+            buttonNode.innerHTML = title;
+
+            if (attr) {
+                if (attr.length === 1) {
+                    buttonNode.setAttribute(attr.key, attr.value);
+                } else if (attr.length > 1) {
+                    attr.forEach(({key, value}) => {
+                        buttonNode.setAttribute(key, value);
+                    });
                 }
-                buttonNode.addEventListener("click", obj.close)
-                obj.templateNode.querySelector(".buttons-container").appendChild(buttonNode)
-            })
-        } else {
-            let buttonNode = document.createElement("button")
-            buttonNode.classList.add("cancel")
-            buttonNode.innerHTML = "Ok"
-            buttonNode.onclick = obj.close
-            obj.templateNode.querySelector(".buttons-container").appendChild(buttonNode)
-        }
+            }
+
+            buttonsContainer.appendChild(buttonNode);
+        });
+
+        buttonsContainer.addEventListener("click", (event) => {
+            const button = event.target.closest("button");
+            if (button) {
+                const buttonIndex = Array.from(buttonsContainer.children).indexOf(button);
+                const {clicked} = buttons[buttonIndex];
+                if (typeof clicked === "function") {
+                    clicked();
+                }
+                if (this.closeOnClick) {
+                    this.close(event);
+                }
+            }
+        });
+    }
+
+
+    updateButtons() {
+        const buttonsContainer = this.templateNode.querySelector(".buttons-container")
+        buttonsContainer.innerHTML = ""
+        const buttons = this.buttons;
+
+        buttons.forEach(({type, title, clicked}) => {
+            const buttonNode = document.createElement("button");
+            buttonNode.classList.add(type);
+            buttonNode.innerHTML = title;
+            if (typeof clicked === "function") {
+                buttonNode.addEventListener("click", clicked);
+            }
+
+            if (this.closeOnClick) {
+                buttonNode.addEventListener("click", this.close.bind(this));
+            }
+
+            buttonsContainer.appendChild(buttonNode);
+        });
     }
 
     show(template) {
-        template.querySelector(".close-modal-container").onclick = this.close
-        this.target.appendChild(template)
-        unfade(template)
+
+        template.querySelector(".close-modal-container").addEventListener("click", this.close.bind(this));
+
+        this.target.appendChild(template);
+
+        unfade(template);
+        adjustHeight(template);
+
     }
 
-    close() {
-        let boxShadow = document.querySelector(".box-shadow")
-        fade(boxShadow)
+    close(event) {
+        const closeButton = event.target;
+        const modal = closeButton.closest(".modal-main");
 
+        if (modal) {
+            const boxShadow = modal.parentNode;
+            fade(boxShadow);
+        }
     }
+
+    setCloseOnClick(closeOnClick) {
+        this.closeOnClick = closeOnClick
+    }
+
 }
 
-
 function unfade(element, display = "flex") {
-    var op = 0.1;  // initial opacity
+    let opacity = 0.1;
+
     element.style.display = display;
-    element.style.opacity = op;
-    var timer = setInterval(function () {
-        if (op >= 1) {
+    element.style.opacity = opacity;
+
+    const timer = setInterval(() => {
+        if (opacity >= 1) {
             clearInterval(timer);
         }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
+        element.style.opacity = opacity;
+        element.style.filter = `alpha(opacity=${opacity * 100})`;
+        opacity += opacity * 0.1;
     }, 6);
 }
 
 function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1) {
+    let opacity = 1;
+
+    const timer = setInterval(() => {
+        if (opacity <= 0.1) {
             clearInterval(timer);
-            element.remove()
+            element.remove();
         }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
+        element.style.opacity = opacity;
+        element.style.filter = `alpha(opacity=${opacity * 100})`;
+        opacity -= opacity * 0.1;
     }, 6);
+}
+
+function adjustHeight(template) {
+    const modalHeight = template.querySelector(".modal-main").offsetHeight;
+    const bodyContainerHeight = template.querySelector(".modal-main .modal-main-container").offsetHeight;
+    const headHeight = template.querySelector(".modal-main header").offsetHeight;
+    const buttonsContainerHeight = template.querySelector(".modal-main .buttons-container").offsetHeight;
+
+    const bodyMaxHeight = modalHeight - headHeight - buttonsContainerHeight;
+
+    if (bodyContainerHeight > bodyMaxHeight) {
+        template.querySelector(".modal-main .modal-main-container").style.height = `${bodyMaxHeight}px`;
+    }
 }
